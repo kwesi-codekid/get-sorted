d5a080a1adbcb4d0353c30fd390e98af3101a5e2
Jacob Kwasi Bordes <kwasibordesjacob@gmail.com>
2024-06-29 16:36:53 +0000

    login page redesign

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,51 +1,77 @@
-import { Button } from "@nextui-org/react";
+import { Button, Card, Link } from "@nextui-org/react";
 import { ActionFunction } from "@remix-run/node";
-import { Form, useActionData, useNavigation } from "@remix-run/react";
+import {
+  Form,
+  useActionData,
+  useNavigate,
+  useNavigation,
+} from "@remix-run/react";
 import PasswordInput from "~/components/inputs/password";
 import TextInput from "~/components/inputs/text";
 import { ActionDataInterface } from "~/utils/types";
 
 export default function Login() {
   const navigation = useNavigation();
+  const navigate = useNavigate();
 
   //   handle form actions
   const actionData = useActionData<typeof action>();
 
   return (
-    <div className="h-screen grid grid-cols-2 gap-8">
+    <div className="h-screen grid grid-cols-2 gap-8 bg-[url('assets/images/black-background-texture.jpeg')] bg-cover bg-no-repeat bg-center">
+      {/* placeholder content */}
+      <div className="h-full bg-transparent"></div>
+
       {/* login form */}
       <div className="h-full flex flex-col gap-8 items-center justify-center">
-        <div className="w-3/5 flex flex-col gap-8">
-          <h2 className="font-montserrat font-bold text-4xl">
+        <Card className="w-3/5 rounded-3xl flex flex-col gap-8 backdrop-blur-lg bg-[#18181b] pt-8 px-8 pb-12 border border-white/5">
+          <h2 className="font-montserrat font-bold text-4xl text-white">
             Login To Your Dashboard
           </h2>
 
-          <Form
-            method="POST"
-            id="login-form"
-            className="pr-12 flex flex-col gap-6"
-          >
-            <TextInput label="Email" name="email" actionData={actionData} />
+          <Form method="POST" id="login-form" className="flex flex-col gap-8">
+            <TextInput
+              label="Email"
+              name="email"
+              actionData={actionData}
+              classNames={{
+                label: "text-white font-sen font-semibold",
+                base: "shadow-none",
+                inputWrapper: "border-gray-600",
+                errorMessage: "font-nunito",
+              }}
+            />
             <PasswordInput
               label="Password"
               name="password"
               actionData={actionData}
+              classNames={{
+                label: "text-white font-sen font-semibold",
+                base: "shadow-none",
+                inputWrapper: "border-gray-600",
+                errorMessage: "font-nunito",
+              }}
             />
+            <div className="flex items-center justify-end">
+              <Link
+                onPress={() => navigate("/reset password")}
+                className="font-nunito text-right"
+              >
+                Forgot password?
+              </Link>
+            </div>
             <Button
               type="submit"
               form="login-form"
               isLoading={navigation.state === "submitting"}
               color="primary"
-              className="font-nunito font-bold"
+              className="font-montserrat font-medium"
             >
               Submit
             </Button>
           </Form>
-        </div>
+        </Card>
       </div>
-
-      {/* placeholder content */}
-      <div className="h-full bg-blue-600"></div>
     </div>
   );
 }
