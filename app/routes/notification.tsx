3d81d1cb0ef000c4dab4348d95e2b35bf5f9ecc6
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2024-06-29 12:51:32 +0000

    more

--- app/routes/notification.tsx
+++ app/routes/notification.tsx
@@ -1,29 +1,29 @@
-import { Button } from "@nextui-org/react";
-import { Notification } from "electron";
-import React from "react";
+// import { Button } from "@nextui-org/react";
+// import { Notification } from "electron";
+// import React from "react";
 
-export default function notification() {
-  return (
-    <div>
-      <Button
-        onClick={() => {
-          new Notification({
-            title: "Notification Title",
-            body: "Notification Body",
-          }).show();
-        }}
-      >
-        Show Notification
-      </Button>
-    </div>
-  );
-}
+// export default function notification() {
+//   return (
+//     <div>
+//       <Button
+//         onClick={() => {
+//           new Notification({
+//             title: "Notification Title",
+//             body: "Notification Body",
+//           }).show();
+//         }}
+//       >
+//         Show Notification
+//       </Button>
+//     </div>
+//   );
+// }
 
-export async function action() {
-  new Notification({
-    title: NOTIFICATION_TITLE,
-    body: NOTIFICATION_BODY,
-  }).show();
+// export async function action() {
+//   new Notification({
+//     title: NOTIFICATION_TITLE,
+//     body: NOTIFICATION_BODY,
+//   }).show();
 
-  return {};
-}
+//   return {};
+// }
--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@nextui-org/react";
 import { LoaderFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import { callNotification } from "~/controllers/noti";
 
 export default function Test() {
   const { user } = useLoaderData<typeof loader>();
@@ -7,11 +9,23 @@ export default function Test() {
     <main>
       <h1 className="font-black text-7xl text-violet-700">Hi, {user}</h1>
       <Link to={"/"}>Home</Link>
+      <Button
+        onClick={() => {
+          const NOTIFICATION_TITLE = "Basic Notification";
+          const NOTIFICATION_BODY = "Notification from the Main process";
+
+          new window.Notification(NOTIFICATION_TITLE, {
+            body: NOTIFICATION_BODY,
+          });
+        }}
+      >
+        Notify
+      </Button>
     </main>
   );
 }
 
-export const loader: LoaderFunction = () => {
+export const loader: LoaderFunction = async () => {
   return {
     user: "Kwasi",
   };
