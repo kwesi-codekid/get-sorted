d322baa66ae407299b3747c0d17dc7e836637b91
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2024-06-29 13:00:58 +0000

    a

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,7 +1,5 @@
-import { Button } from "@nextui-org/react";
 import { LoaderFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import { callNotification } from "~/controllers/noti";
 
 export default function Test() {
   const { user } = useLoaderData<typeof loader>();
@@ -9,23 +7,11 @@ export default function Test() {
     <main>
       <h1 className="font-black text-7xl text-violet-700">Hi, {user}</h1>
       <Link to={"/"}>Home</Link>
-      <Button
-        onClick={() => {
-          const NOTIFICATION_TITLE = "Basic Notification";
-          const NOTIFICATION_BODY = "Notification from the Main process";
-
-          new window.Notification(NOTIFICATION_TITLE, {
-            body: NOTIFICATION_BODY,
-          });
-        }}
-      >
-        Notify
-      </Button>
     </main>
   );
 }
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = () => {
   return {
     user: "Kwasi",
   };
