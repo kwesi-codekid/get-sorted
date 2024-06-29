700d4057402987fb26ebbb1aacc530280d96cee7
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2024-06-29 13:04:56 +0000

    more

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@nextui-org/react";
 import { LoaderFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 
@@ -7,6 +8,18 @@ export default function Test() {
     <main>
       <h1 className="font-black text-7xl text-violet-700">Hi, {user}</h1>
       <Link to={"/"}>Home</Link>
+      <Button
+        onClick={() => {
+          const NOTIFICATION_TITLE = "Basic Notification";
+          const NOTIFICATION_BODY = "Notification from the Main process";
+
+          new window.Notification(NOTIFICATION_TITLE, {
+            body: NOTIFICATION_BODY,
+          });
+        }}
+      >
+        Notify
+      </Button>
     </main>
   );
 }
