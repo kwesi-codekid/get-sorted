87506254036f56b28a26be29686f16b706a58edb
Jacob Kwasi Bordes <kwasibordesjacob@gmail.com>
2024-07-04 09:44:06 +0000

    realtime

--- app/root.tsx
+++ app/root.tsx
@@ -6,12 +6,17 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
 } from "@remix-run/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import styles from "./styles.css";
 import snow from "react-quill/dist/quill.snow.css";
-import { NextUIProvider } from "@nextui-org/react";
+import { Button, NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
+import { ArrowLeftAnimated } from "./components/icons/arrows";
+import errorIllustration from "~/assets/animated/503-error-animate.svg";
 
 export const meta: MetaFunction = () => [
   { title: "GetSorted Helpdesk System" },
@@ -47,4 +52,80 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className="pt-16  h-full">
+        <div className="bg-red-500/10 h-full flex flex-col gap-6">
+          <img
+            src={errorIllustration}
+            alt="Error Illustration"
+            className="w-1/3"
+          />
+          <div>
+            <h1 className="font-montserrat font-extrabold text-5xl text-red-500 text-center">
+              {error.status} {error.statusText}
+            </h1>
+            <p className="font-nunito text-center text-lg">{error.data}</p>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <Button
+              color="danger"
+              className="font-montserrat font-medium"
+              onPress={() => navigate(-1)}
+              size="sm"
+              startContent={<ArrowLeftAnimated className="size-5" />}
+            >
+              Go Back
+            </Button>
+            <p className="font-nunito text-sm">
+              Please contact the IT Team if the issue persists.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div className="pt-14 h-full">
+        <div className="bg-red-500/10 dark:bg-red-500/15 rounded-2xl h-full overflow-y-auto vertical-scrollbar flex flex-col gap-6 items-center justify-center">
+          <img
+            src={errorIllustration}
+            alt="Error Illustration"
+            className="w-1/3"
+          />
+          <div>
+            <h1 className="font-montserrat font-extrabold text-5xl text-red-500 text-center">
+              Unexpected Error!
+            </h1>
+            <p className="font-nunito text-center text-lg line-clamp-2">
+              {error.message}
+            </p>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button
+              color="danger"
+              className="font-montserrat font-medium"
+              onPress={() => navigate("/")}
+              size="sm"
+              startContent={<ArrowLeftAnimated className="size-5" />}
+            >
+              Go Back
+            </Button>
+            <p className="font-nunito text-sm">
+              Please contact the IT Team if the issue persists.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,9 +1,17 @@
-import { Outlet, useNavigate } from "@remix-run/react";
+import { Button } from "@nextui-org/react";
+import {
+  Outlet,
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from "@remix-run/react";
 import { useEffect, useState } from "react";
+import { ArrowLeftAnimated } from "~/components/icons/arrows";
 import Header from "~/components/sections/header";
 import Sidebar from "~/components/sections/sidebar";
 import { adminNavLinks } from "~/data/nav-links";
 import { useLocalStorage } from "~/hooks/useLocalStorage";
+import errorIllustration from "~/assets/animated/503-error-animate.svg";
 
 export default function AdminLayout() {
   const navigate = useNavigate();
@@ -39,4 +47,80 @@ export default function AdminLayout() {
       </div>
     </div>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className="pt-16  h-full">
+        <div className="bg-red-500/10 h-full flex flex-col gap-6">
+          <img
+            src={errorIllustration}
+            alt="Error Illustration"
+            className="w-1/3"
+          />
+          <div>
+            <h1 className="font-montserrat font-extrabold text-5xl text-red-500 text-center">
+              {error.status} {error.statusText}
+            </h1>
+            <p className="font-nunito text-center text-lg">{error.data}</p>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <Button
+              color="danger"
+              className="font-montserrat font-medium"
+              onPress={() => navigate(-1)}
+              size="sm"
+              startContent={<ArrowLeftAnimated className="size-5" />}
+            >
+              Go Back
+            </Button>
+            <p className="font-nunito text-sm">
+              Please contact the IT Team if the issue persists.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div className="pt-14 h-full">
+        <div className="bg-red-500/10 dark:bg-red-500/15 rounded-2xl h-full overflow-y-auto vertical-scrollbar flex flex-col gap-6 items-center justify-center">
+          <img
+            src={errorIllustration}
+            alt="Error Illustration"
+            className="w-1/3"
+          />
+          <div>
+            <h1 className="font-montserrat font-extrabold text-5xl text-red-500 text-center">
+              Unexpected Error!
+            </h1>
+            <p className="font-nunito text-center text-lg line-clamp-2">
+              {error.message}
+            </p>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button
+              color="danger"
+              className="font-montserrat font-medium"
+              onPress={() => navigate(-1)}
+              size="sm"
+              startContent={<ArrowLeftAnimated className="size-5" />}
+            >
+              Go Back
+            </Button>
+            <p className="font-nunito text-sm">
+              Please contact the IT Team if the issue persists.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
